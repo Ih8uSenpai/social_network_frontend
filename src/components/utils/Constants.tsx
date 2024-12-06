895519163e1c37837e26582b9e806a61186fd948
Ih8uSenpai <mipichikimicci33uwu@gmail.com>
2024-12-06 04:22:53 +0300

    upd

--- src/components/utils/Constants.tsx
+++ src/components/utils/Constants.tsx
@@ -1,8 +1,8 @@
 import chatIcon from '../resources/images/no_image_icon.jpg'
-export const defaultProfileIcon = `${process.env.REACT_APP_STATIC_URL}/standart_icon.jpg`;
-export const defaultBannerImage = `${process.env.REACT_APP_STATIC_URL}/standart_banner.jpg`;
+export const defaultProfileIcon = "/standart_icon.jpg";
+export const defaultBannerImage = "/standart_banner.jpg";
 export const defaultChatIcon = chatIcon;
-export const defaultMusicIcon = `${process.env.REACT_APP_STATIC_URL}/defaultMusicIcon.jpg`;
+export const defaultMusicIcon = "/defaultMusicIcon.jpg";
 export const photo_box_style = {
     position: 'absolute',
     top: '50%',
